9dd7a4aaa1d0381094df34b46f77cc3871476447
wata712 <83803356+wata712@users.noreply.github.com>
2021-05-08 00:24:10 +0900

    main

--- EvaluationDisplay_6.py
+++ EvaluationDisplay_6.py
@@ -1,11 +1,4 @@
 import csv
-def takara2(i=1):
-  print(data[i])
-  
-
-def takara():
-  #ハローワールドを出力する
-  print("hello world")
 
 data = []
 
@@ -20,11 +13,9 @@ with open("成績サンプル-UTF8.csv","r") as file:
 
 print(len(data))
 
-
-
 print(data[0][1])
-takara()
 
-takara2()
+for i in range(len(data)):
+  sum += data[i][1]
 
-#test
+print(data)
