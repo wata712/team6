a27c3969c3b21db49dfea26c86846fb34422e7e9
wata712 <83803356+wata712@users.noreply.github.com>
2021-05-08 11:08:08 +0900

    main

--- EvaluationDisplay_6.py
+++ EvaluationDisplay_6.py
@@ -1,5 +1,20 @@
 import csv
 
+def ave():
+  sum = 0
+  for i in range(len(data)):
+    sum += int(data[i][1])
+  return sum
+
+def hyoujunn():
+  h = 0
+  for i in range(len(data)):
+    a = float(data[i][1])
+    h += (heikinn-a)**2
+  ans = h/len(data)
+  return ans
+
+
 data = []
 
 with open("成績サンプル-UTF8.csv","r",encoding="utf-8") as file:
@@ -9,11 +24,9 @@ with open("成績サンプル-UTF8.csv","r",encoding="utf-8") as file:
   for row in reader:
     data.append(row)
 
-print(len(data))
+sum2 = ave()
+heikinn = sum2/len(data)
+stde = hyoujunn()
 
-print(data[0][1])
-
-for i in range(len(data)):
-  sum += 1
-
-print(sum)
+print("平均は" + str(heikinn))
+print("標準偏差は"+str(stde))
