e5bd7474408c7c8427918d84277aa413cef8f0cd
nihachi16 <83803353+nihachi16@users.noreply.github.com>
2021-06-16 23:48:57 +0900

    main

--- MainProject/main.py
+++ MainProject/main.py
@@ -4,11 +4,11 @@ eel.init("MainProject/view")
 eel.start("main.html", block=False)
 
 @eel.expose
-def send(msg):
-    print("Received Message: " + msg)
-    return "ok"
+def registtData(tData):
+   tID = tData[0]
+   tPW = tData[1]
+   print("tID: {0} tPW: {1}".format(tID, tPW))
 
+#これがないと動かない
 while True:
-    text = eel.readTextBox()()
-    print("Text box contents: {}".format(text))
     eel.sleep(2.0)
