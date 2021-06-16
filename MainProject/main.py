8cb28d3533756f2c6c2bb87f6e095a1eac5ea7b9
nihachi16 <83803353+nihachi16@users.noreply.github.com>
2021-06-17 00:11:28 +0900

    main

--- MainProject/main.py
+++ MainProject/main.py
@@ -1,4 +1,5 @@
 import eel
+import csv
 
 eel.init("MainProject/view")
 eel.start("main.html", block=False)
@@ -9,6 +10,16 @@ def registtData(tData):
    tPW = tData[1]
    print("tID: {0} tPW: {1}".format(tID, tPW))
 
-#これがないと動かない
+
+#教員ファイル読み込み
+csv_file = open("./data/教員・担当科目リスト.csv", "r", encoding="utf_8", errors="", newline="" )
+f = csv.DictReader(csv_file)
+for row in f:
+    #rowはdictionary
+    #row["column_name"] or row.get("column_name")で必要な項目を取得することができる
+    print(row)
+
+#これがないと動かないんでよ
 while True:
     eel.sleep(2.0)
+
