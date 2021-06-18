bf6c8e4fc317c78fe0a5730deee0bb6e6a51a23c
nihachi16 <83803353+nihachi16@users.noreply.github.com>
2021-06-19 00:04:54 +0900

    main

--- MainProject/main.py
+++ MainProject/main.py
@@ -5,19 +5,46 @@ eel.init("MainProject/view")
 eel.start("main.html", block=False)
 
 @eel.expose
-def registtData(tData):
-   tID = tData[0]
-   tPW = tData[1]
-   print("tID: {0} tPW: {1}".format(tID, tPW))
+def registtData():
+   registtDatatoPy()
+
+def registtDatatoPy():
+    tData = eel.sendtDatatoPy()()
+    tID = tData[0]
+    tPW = tData[1]
+    print("tID: {0} tPW: {1}".format(tID, tPW))
+    tIDtPWverify(tID,tPW)
+
+#教員ファイル読み込み/tID,yPW照合
+def tIDtPWverify(tID,tPW):
+    tnamecsv = {}
+    with open("./data/教員・担当科目リスト.csv", "r", encoding="utf_8", errors="", newline="") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            tnamecsv[row["ID"]] = row["氏名"]
+    print(tnamecsv[tID])
+
+    tpwcsv = {}
+    with open("./data/tPW.csv","r")as p:
+        reader = csv.DictReader(p)
+        for prow in reader:
+            tpwcsv[prow["tID"]] = prow["tPW"]
+        tPWoncsv = tpwcsv[tID]
+    #print(tPWoncsv)
+
+    if tPW == tPWoncsv:
+        print("Yeeeeeeeeee")
+        return True
+    else:
+        print("noooooooooo")
+        return False
+
+    
 
 
-#教員ファイル読み込み
-csv_file = open("./data/教員・担当科目リスト.csv", "r", encoding="utf_8", errors="", newline="" )
-f = csv.DictReader(csv_file)
-for row in f:
-    #rowはdictionary
-    #row["column_name"] or row.get("column_name")で必要な項目を取得することができる
-    print(row)
+
+
+
 
 #これがないと動かないんでよ
 while True:
