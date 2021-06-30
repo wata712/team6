9de5c9400447411d86f80f62203986300c839a1c
nihachi16 <83803353+nihachi16@users.noreply.github.com>
2021-06-30 16:56:10 +0900

    main

--- MainProject/main.py
+++ MainProject/main.py
@@ -75,57 +75,60 @@ def pickcName():
 
     # tccsv = [[0] * 5 for i in range(4)]
     # print(tccsv)
-    tcName = [[0] * 5 for i in range(4)]
-    tccsvx = []
-    for i in range(5):
-        with open("./data/教員・担当科目リスト.csv", "r", encoding="utf_8", errors="", newline="") as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                print(row)
-                tanto = str('担当科目' + str(i+1))
-                print(tanto)
-                tccsvx[row["ID"]] = row[tanto]
-            tcName[i] = str(tccsvx[tID])
-            print("calss1: " + tcName[i])
+    # tcName = [[0] * 5 for i in range(4)]
+    # tccsvx = []
+    # for i in range(5):
+    #     with open("./data/教員・担当科目リスト.csv", "r", encoding="utf_8", errors="", newline="") as f:
+    #         reader = csv.DictReader(f)
+    #         for row in reader:
+    #             print(row)
+    #             tanto = str('担当科目' + str(i+1))
+    #             print(tanto)
+    #             tccsvx[row["ID"]] = row["担当科目1"]
+    #         tcName[i] = str(tccsvx[tID])
+    #         print("calss1: " + tcName[i])
         
 
-    # tc1csv = {}
-    # tc2csv = {}
-    # with open("./data/教員・担当科目リスト.csv", "r", encoding="utf_8", errors="", newline="") as f:
-    #     reader = csv.DictReader(f)
-    #     for row in reader:
-    #         tc1csv[row["ID"]] = row["担当科目1"]
-    #         tc2csv[row["ID"]] = row["担当科目2"]
-    # tc1Name = str(tc1csv[tID])
-    # tc2Name = str(tc2csv[tID])
-    # print("calss1: " + tc1Name)
-    # print("calss2: " + tc2Name)
+    tc1csv = {}
+    tc2csv = {}
+    with open("./data/教員・担当科目リスト.csv", "r", encoding="utf_8", errors="", newline="") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            tc1csv[row["ID"]] = row["担当科目1"]
+            tc2csv[row["ID"]] = row["担当科目2"]
+    tc1Name = str(tc1csv[tID])
+    tc2Name = str(tc2csv[tID])
+    print("calss1: " + tc1Name)
+    print("calss2: " + tc2Name)
 
-    tcID = [[0] * 5 for i in range(4)]
-    tcxID = [[0] * 5 for i in range(4)]
-    for j in range(5):
-        with open("./data/講義科目ルール.csv", "r", encoding="utf_8", errors="", newline="") as p:
-            reader = csv.DictReader(p)
-            for row in reader:
-                tcxID[i][row["科目名"]] = row["講義ID"]
-            tcID[j] = str(tcxID[tcName[j]])
-            print("classID: " + tcID[j])
+    # tcID = [[0] * 5 for i in range(4)]
+    # tcxID = [[0] * 5 for i in range(4)]
+    # for j in range(5):
+    #     with open("./data/講義科目ルール.csv", "r", encoding="utf_8", errors="", newline="") as p:
+    #         reader = csv.DictReader(p)
+    #         for row in reader:
+    #             tcxID[j][row["科目名"]] = row["講義ID"]
+    #         tcID[j] = str(tcxID[tc1Name])
+    #         print("classID: " + tcID[j])
 
 
-    # tc1xID = {}
-    # tc2xID = {}
-    # with open("./data/講義科目ルール.csv", "r", encoding="utf_8", errors="", newline="") as p:
-    #     reader = csv.DictReader(p)
-    #     for row in reader:
-    #         tc1xID[row["科目名"]] = row["講義ID"]
-    #         tc2xID[row["科目名"]] = row["講義ID"]
-    # tc1ID = str(tc1xID[tc1Name])
-    # tc2ID = str(tc2xID[tc2Name])
-    # print("calss1ID: " + tc1ID)
-    # print("calss2ID: " + tc2ID)
+    tc1xID = {}
+    tc2xID = {}
+    with open("./data/講義科目ルール.csv", "r", encoding="utf_8", errors="", newline="") as p:
+        reader = csv.DictReader(p)
+        for row in reader:
+            tc1xID[row["科目名"]] = row["講義ID"]
+            tc2xID[row["科目名"]] = row["講義ID"]
+    tc1ID = str(tc1xID[tc1Name])
+    try:
+        tc2ID = str(tc2xID[tc2Name])
+    except(KeyError):
+        tc2ID = "X0"
+    print("calss1ID: " + tc1ID)
+    print("calss2ID: " + tc2ID)
 
-    tcDay = [5]
-    tcPeriod = [5]
+    tcDay = [0, 0]
+    tcPeriod = [0, 0]
     cID = [tc1ID, tc2ID]
     for n in range(0, len(cID)):
         print(n)
@@ -156,11 +159,14 @@ def pickcName():
         else:
             print('Class period config error')
 
-        print(tcDay[n] + tcPeriod[n])
+        try:
+            print(tcDay[n] + tcPeriod[n])
+        except(IndexError):
+            pass
         n = n+1
 
 
-    eel.addcData(tc1Name, tc2Name, tc1ID, tc2ID)
+    eel.addcData(tc1Name, tc2Name, tcDay, tcPeriod)
 
 
 #これがないと動かないんでよ
