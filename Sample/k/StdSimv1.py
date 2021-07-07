22edcbbaab2b5a359ca64735736d764fcf984750
nihachi16 <83803353+nihachi16@users.noreply.github.com>
2021-07-07 16:46:29 +0900

    main

---
+++ Sample/k/StdSimv1.py
@@ -0,0 +1,16 @@
+import csv
+
+def stdSim(cID):
+    #講義IDに一致した履修者csvを開く
+    stdcsvName = "./data/履修者-" + cID + ".csv"
+    with open(stdcsvName, "r", encoding="utf_8", errors="", newline="") as p:
+        reader = csv.DictReader(p)
+
+    ## codearea ##
+    #cID 講義ID
+    #IDm IDm
+    #StdID 学籍番号
+    #StdName 名前
+    #inTime 入室時間(正規分布で生成して)
+
+    return (IDm, StdID, StdName, inTime)
