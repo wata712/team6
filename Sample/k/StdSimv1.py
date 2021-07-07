f11f7467818942ebf14f0e356ee296e2c2e2cdde
nihachi16 <83803353+nihachi16@users.noreply.github.com>
2021-07-07 17:35:29 +0900

    main

--- Sample/k/StdSimv1.py
+++ Sample/k/StdSimv1.py
@@ -1,17 +1,36 @@
 import csv
 
 def stdSim(cID):
+    #学籍番号を(ランダムに)生成
+    for i in range(1, 101):
+        temNo = "S" + str(i)
+        print(temNo)
+        #if文とかで"S001" "S012"のように3桁表示になるようにして
+
+    stdIDmx = {} #辞書型
+    stdNamex = {}
+    stdID = [] #配列
+    stdName = []
     #講義IDに一致した履修者csvを開く
     stdcsvName = "./data/履修者-" + cID + ".csv"
     with open(stdcsvName, "r", encoding="utf_8", errors="", newline="") as p:
         reader = csv.DictReader(p)
+        for row in reader:
+            stdIDmx[row["学籍番号"]] = row["IDm"]
+            stdNamex[row["学籍番号"]] = row["名前"]
+    stdID = str(stdIDmx[temNo])
+    stdName = str(stdNamex[temNo])
+    print(stdID)
+    print(stdName)
 
-    ## codearea ##
+    #上のかたまり周辺でfor文とか使ってうまいことやって
+
+    ## memoarea ##
     #cID 講義ID
-    #IDm IDm
-    #StdID 学籍番号
-    #StdName 名前
-    #inTime 入室時間(講義開始10分前を中心として定数とか使った正規分布で生成して)
+    #IDm IDm csvママ 1次元配列
+    #StdID 学籍番号 csvママ 1次元配列
+    #StdName 名前 csvママ 1次元配列
+    #inTime 入室時間 1次元配列 個々の形式:str(hh:mm:ss) 例 inTime[0] : 08:43:21 (講義開始10分前を中心として定数とか使った正規分布で生成して)
 
     return (IDm, StdID, StdName, inTime)
 
