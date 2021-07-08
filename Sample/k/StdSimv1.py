1f3f53ec3da2a5478081ea6fec943d56c3dd9b80
nihachi16 <83803353+nihachi16@users.noreply.github.com>
2021-07-09 00:13:27 +0900

    main

--- Sample/k/StdSimv1.py
+++ Sample/k/StdSimv1.py
@@ -1,7 +1,7 @@
 import csv
 
 def stdSim(cID):
-    #学籍番号を(ランダムに)生成
+    #学籍番号を(ランダムに)生成して
     for i in range(1, 101):
         temNo = "S" + str(i)
         print(temNo)
@@ -15,17 +15,18 @@ def stdSim(cID):
         reader = csv.DictReader(p)
         for row in reader:
             stdIDmx[row["学籍番号"]] = row["IDm"]
-    stdIDm = str(stdIDmx[temNo])
+    for i in range(len(row)):
+        stdIDm = str(stdIDmx[temNo])
     print(stdIDm)
 
     #上のかたまり周辺でfor文とか使ってうまいことやって
+    #雛形には多分エラーあるからうまいこといじって
 
     ## memoarea ##
     #cID 講義ID
     #IDm IDm csvママ 1次元配列
-    
 
-    ###！！！ 以下のinTimeは実装しなくていいです ！！！###
+    ###！！！ 以下の変数は実装しなくていいです ！！！###
     #inTime 入室時間 1次元配列 個々の形式:str(hh:mm:ss) 例 inTime[0] : 08:43:21 (講義開始10分前を中心として定数とか使った正規分布で生成)
     #StdID 学籍番号 csvママ 1次元配列
     #StdName 名前 csvママ 1次元配列
