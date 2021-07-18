7fd52ca5bba6cee487897fc84f5cc8534121e53e
nihachi16 <83803353+nihachi16@users.noreply.github.com>
2021-07-19 00:02:04 +0900

    main

--- MainProject/main.py
+++ MainProject/main.py
@@ -1,5 +1,4 @@
 ### Team6 main.py ###
-### ブラウザウィンドウの推奨アスペクト比 4:3 または 16:9 または 21:9 ###
 
 import os
 from re import S
@@ -14,13 +13,16 @@ import datetime
 from datetime import datetime as dt
 import numpy
 import random
+import matplotlib.pyplot as plt
+import glob
+
 # import importer
 # import exporter
 
 # P000の初期PWは000b
 
 eel.init("MainProject/view")
-eel.start("login.html", block=False)
+eel.start("login.html", size=(800, 480), block=False)
 
 @eel.expose
 def registtData():
@@ -313,7 +315,7 @@ def stdSim(cID):
 
 IOcsvName = "xx"
 
-#出席リストCSV作成
+#出欠リストCSV操作
 @eel.expose
 def openIOcsv(cID, cName):
     global datew
@@ -478,9 +480,6 @@ def openIOcsv(cID, cName):
                 eel.sleep(3)
             print(stdIDm[s])
             touchIDcard(s, stdlen)
-
-    
-
 
 
 @eel.expose
@@ -670,7 +669,72 @@ def createCumulativeClassGraph(cName):
         atd_count_list2=[]
         
 
+        for key2, value2 in count2.items():
+            att_counter2='{}: {:d}'.format(key2,value2)
+            #学番と出席数リスト
+            #alatd_list.append(att_counter)
+            #学番リスト
+            stnumb_list2.append('{}'.format(key2))
+            #出席数リスト
+            atd_count_list2.append(int(value2))
+        #print(stnumb_list)
+        #print(atd_count_list)
+    count3 = {}
+    for atdlist_csvdata in csv_list3:
+        
+        with open(atdlist_csvdata,encoding='UTF8') as f5:
+            atl_reader5 = csv.reader(f5)
+            atl_header5 = next(atl_reader5)
+            #print(atl_header3)
+            for row in atl_reader5:
+                data5=row[0]
+                data6=row[4]
+                count3.setdefault(data5,0)
+                if '欠席' in data6:
+                    count3[data5] +=1
+        
+        #alatd_list=[]
+        stnumb_list3=[]
+        atd_count_list3=[]
+        
 
+        for key3, value3 in count3.items():
+            att_counter3='{}: {:d}'.format(key3,value3)
+            #学番と出席数リスト
+            #alatd_list.append(att_counter)
+            #学番リスト
+            stnumb_list3.append('{}'.format(key3))
+            #出席数リスト
+            atd_count_list3.append(int(value3))
+        #print(stnumb_list)
+        #print(atd_count_list)
+    #人数
+    list_length=len(stnumb_list)
+    print(list_length)
+
+    #リストの先頭('出席'と出席数)を削除
+    #stnumb_list.remove('出席')
+
+    #atd_count_list.remove(list_length)
+
+
+    #print(alatd_list)
+    #print(stnumb_list)
+    #print(atd_count_list)
+    #print(stnumb_list2)
+    #print(atd_count_list2)
+
+    #↓ここから棒グラフ作成
+    fig=plt.figure()
+    #学生の数,0から連続した整数のリスト
+    y_set=list(range(list_length))
+
+    graph1=plt.bar(y_set,atd_count_list,align="edge",width=-0.5,color="green",label="出席")
+    graph2=plt.bar(y_set,atd_count_list2,align="center",width=0.5,color="blue",label="遅刻")
+    graph3=plt.bar(y_set,atd_count_list3,align="edge",width=0.5,color="red",label="欠席")
+    plt.xticks(y_set,stnumb_list,rotation=90)
+    plt.legend()
+    plt.show()
 
 #これがないと動かないんでよ
 while True:
