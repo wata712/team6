53f176a1ca7333a9a006fdef61ae1df24423353b
nihachi16 <83803353+nihachi16@users.noreply.github.com>
2021-07-25 23:50:04 +0900

    main

---
+++ MainProject/dist/main/server/risyuuTh34_ex.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import csv
+import MySQLdb
+
+
+def fsync():
+  connection = MySQLdb.connect(host="localhost",db="team6",user="root",passwd="",charset="utf8")
+  cursor=connection.cursor()
+
+  cursor.execute("DELETE FROM th34")
+
+  #path変更忘れずに
+  f = open(".\\MainProject\\data\\履修者-Th34_.csv", "r", encoding="utf-8")
+
+  reader = csv.reader(f)
+  header = next(reader)
+  for row in reader:
+    sql = "INSERT IGNORE INTO th34 values(%s,%s,%s,%s,%s)"
+    cursor.execute(sql, (row[0], row[1], row[2],row[3],row[4]))
+  f.close()
+
+  connection.commit()
+
+  cursor.close()
+  connection.close()
