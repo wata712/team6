95539dab9e6d19da409f5d8375c9cb0b48c23033
wata712 <83803356+wata712@users.noreply.github.com>
2021-07-27 12:12:45 +0900

    main

--- MainProject/server/gakusei_ex.py
+++ MainProject/server/gakusei_ex.py
@@ -11,7 +11,7 @@ def fsync():
   cursor.execute("DELETE FROM gakusei")
 
   #path変更忘れずに
-  f = open(".\\MainProject\\data\\学生リスト.csv", "r", encoding="utf-8")
+  f = open("..\\data\\学生リスト.csv", "r", encoding="utf-8")
 
   reader = csv.reader(f)
   header = next(reader)
@@ -24,3 +24,5 @@ def fsync():
 
   cursor.close()
   connection.close()
+
+fsync()
